26a748b402511d5d6936a55b856dac653a6e26f0
srikanth jinikiri <sri.jinikiri6@gmail.com>
2022-06-22 11:44:28 +0530

    Linked map changes

--- src/LinkedHashMaps_Practical.java
+++ src/LinkedHashMaps_Practical.java
@@ -5,8 +5,9 @@ public class LinkedHashMaps_Practical {
 	public static void main(String[] args) {
 		
 		LinkedHashMap<String,Integer> lhm=new LinkedHashMap<>(2,0.6f);
-		lhm.put("srikanth", 123);
-		lhm.put("sds", 121);
+		//Adding surname
+		lhm.put("srikanth_jinikiri", 123);
+		lhm.put("Raghupathi", 121);
 		System.out.println(lhm);
 		
 
